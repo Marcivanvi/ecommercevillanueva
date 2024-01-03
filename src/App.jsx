190d27a7bdbfa557cf0b4a640cf5f9739e2bf594
Marcos Villanueva <Villanuevamarcosivan@gmail.com>
2024-01-03 19:57:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
+
+const App = () => {
+  return (
+    <div>
+      <ItemListContainer saludo = "Hola profesor/tutor espero aprobar jaja"/>
+    </div>
+  )
+}
+
+export default App
